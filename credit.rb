1d33f647906acc98ec394cffc980b9da1efcc211
Shomari <Shomari.Ewing@gmail.com>
2015-06-22 09:53:13 -0500

    tweaking challenge 3

--- credit.rb
+++ credit.rb
@@ -1,4 +1,5 @@
 require 'date'
+require 'pry'
 
 class Credit
 	attr_accessor :apr, :limit, :balance, :ledger
@@ -8,25 +9,29 @@ class Credit
 		@limit = limit
 		@balance =  0
 		@ledger = {}
-		@intrest = []
+		@interest = []
 	end
 
 	def draw(amount)
 		calculate_interest(days_since_transaction)
 		@balance = @balance + amount
-		@ledger[DateTime.now] = @balance
+		@ledger[Date.today] = @balance
 	end
 
 	def payment(amount)
 		calculate_interest(days_since_transaction)		
 		@balance = @balance - amount
-		@ledger[DateTime.now] = @balance
+		@ledger[Date.today] = @balance
 	end
 
 	# Need this to calculate the interest owed for each time period
 	def days_since_transaction
-		last_date = @ledger.keys.last
-		days = Time.now - last_date
+		if @ledger.empty?
+			return 1
+		else
+			last_date = @ledger.keys.last 
+		end
+		days = Date.today - last_date		
 	end
 
 	# Everytime there is a trancastion, calculate intrest owed since
@@ -38,6 +43,11 @@ class Credit
 	# Would have a cron job run this at the end of each month
 	# Calculate's total interest for the bill
 	def end_of_month_bill
+		calculate_interest(30)  ### 30 should be replaced by days since last transaction
 		@balance + @interest.reduce(:+)
 	end
 end
+
+sho = Credit.new(0.35, 1000)
+p sho.draw(300)
+p sho.end_of_month_bill
